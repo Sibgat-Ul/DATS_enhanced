366552a10be623d1301c1c26024b93d30f7daae7
Sibgat-Ul_Islam <sibgatulislam@gmail.com>
2025-04-04 23:41:22 +0600

    debug

--- mdistiller/distillers/KD.py
+++ mdistiller/distillers/KD.py
@@ -39,7 +39,7 @@ class KD(Distiller):
         else:
             logits_student = self.student(image)
             logits_teacher = self.teacher(image)
-            print(logits_student.shape, target.shape)
+            print(target)
         # losses
         student_loss = F.cross_entropy(logits_student, target)
         teacher_loss = F.cross_entropy(logits_teacher, target)
